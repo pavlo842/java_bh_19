449adfcfe96a026d3522ab8d8538b98687bb640f
pavlo842 <pavlo842@mail.ru>
2019-12-10 20:09:10 +0300

    lessons-2

---
+++ hello_world/src/by/belhard/j19/lessons/lesson2/PrimitivesExamples.java
@@ -0,0 +1,24 @@
+package by.belhard.j19.lessons.lesson2;
+// by.belhard.j19.lessons.lesson2.PrimitivesExamples  -  полное название пакета
+public class PrimitivesExamples {
+    // psvm + enter - public static void main(String[] args) {}
+    // COMMENTS
+    // ctrl + / - однострочный
+    // ctrl + shift + / - многострочный
+    public static void main(String[] args) {
+
+/*        short sh = 5;
+        int i = 100;
+        double d = 3.14;
+
+        System.out.println(sh + i / d);*/
+
+/*      int i = 0;
+
+        System.out.println(++i);
+        System.out.println(i++);
+        System.out.println(i);*/
+
+
+    }
+}
--- hello_world/src/hello_world.java
+++ hello_world/src/hello_world.java
@@ -2,5 +2,7 @@ public class hello_world {
     public static void main(String[] args) {
         System.out.println(5 + 2);
         System.out.println("Hello" + " world");
+        char b = 'c' - 10;
+        System.out.println(b);
     }
 }
