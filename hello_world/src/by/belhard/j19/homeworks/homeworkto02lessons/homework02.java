df22cc22dca87688456258ae9359a3b3e399ccc9
Pavel <pishchalaups@belhard.com>
2019-12-12 16:40:46 +0300

    homework02

--- hello_world/src/by/belhard/j19/homeworks/homeworkto02lessons/homework02.java
+++ hello_world/src/by/belhard/j19/homeworks/homeworkto02lessons/homework02.java
@@ -1,4 +1,23 @@
 package by.belhard.j19.homeworks.homeworkto02lessons;
 
 public class homework02 {
+    public static void main(String[] args) {
+        int i = 158;
+        int ii = 15;
+        int as = ii + 15;
+        double k = 24;
+        System.out.println(i * as % k);
+        System.out.println(i >= 100); // true
+        System.out.println(i <= 100); // false
+        System.out.println(k != ii);  // true
+
+        System.out.println(++ii); // 16
+
+        System.out.println((ii + k)); // System.out.println(++ii); + 20 = 40.0
+
+        int r;
+        r = 4;
+        double s = 3.14;
+        System.out.println((r * r) * s);
+    }
 }
