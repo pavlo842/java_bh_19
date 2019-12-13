52f519ee2991e061a2e672baf8222070e73600aa
pavlo842 <pavlo842@mail.ru>
2019-12-13 19:57:50 +0300

    lesson 3

---
+++ hello_world/src/by/belhard/j19/lessons/lesson3/LessonsThree.java
@@ -0,0 +1,52 @@
+package by.belhard.j19.lessons.lesson3;
+
+public class LessonsThree {
+
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 7;
+
+/*        if (a > b) {
+            System.out.println(a + "more than " + b);
+        } else {
+            System.out.println("Variable a not more b");
+        }*/
+            a = 8;
+/*
+        if (a > b + b) {
+            System.out.println(a + " more than " + (b + b));
+        } else {
+            System.out.println("Variable a not more b");
+        }
+*/
+/*        String result;
+
+        if (a > 10) {
+            result = "Yes";
+        } else if (a < 10) {
+            result = "No";
+        } else {
+            result = "Good";
+        }
+        System.out.println(result);*/
+
+/*        int f = 5;
+
+        while (f > 0) {
+
+            System.out.println(f);
+
+            f--;
+        }
+
+        System.out.println("START");*/
+
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+
+}
---
+++ hello_world/src/by/belhard/j19/lessons/lesson3/LessonsThreeTwo.java
@@ -0,0 +1,21 @@
+package by.belhard.j19.lessons.lesson3;
+
+import java.sql.SQLOutput;
+
+public class LessonsThreeTwo {
+
+    public static void main(String[] args) {
+
+//        for (int i = 0, g = 1; i < 5 & g > 10; i++, g++) {}
+
+        for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < 12; i++) {
+                System.out.print("a");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
