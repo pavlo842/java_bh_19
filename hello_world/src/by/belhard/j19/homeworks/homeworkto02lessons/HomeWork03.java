c54daa63734cc12f9118dd3d40ef6116dbac5e09
Pavel <pishchalaups@belhard.com>
2019-12-16 14:33:53 +0300

    home work 3 part 1

---
+++ hello_world/src/by/belhard/j19/homeworks/homeworkto02lessons/HomeWork03.java
@@ -0,0 +1,90 @@
+package by.belhard.j19.homeworks.homeworkto02lessons;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class HomeWork03 {
+
+    public static void main(String[] args) {
+
+        // Задача 1
+
+/*        int t = -15000;
+
+        String clothes = "";
+
+        if (t < 10) {
+            clothes = "Куртка";
+        } else if (t > 10 && t < 15) {
+            clothes = "Ветровка";
+        } else if (t > 16 && t < 20) {
+            clothes = "Свитер";
+        } else if (t > 21 && t < 30) {
+            clothes = "Майка";
+        } else {
+            clothes = "Ничего";
+        }
+
+        System.out.println(clothes);*/
+
+
+        // Задача 3
+        // degree = 2 * 2 * 2
+
+/*        int degree = 1;
+
+        for (int i = 0; i < 3; i++) {
+            degree *= 2;
+        }
+        System.out.print(degree);*/
+
+
+        // Задача 5
+
+/*        int i;
+        for (i = 1; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 7 == 0))
+            System.out.println(i + " ");
+        }*/
+
+        // Задача 5 доп 1
+
+/*        int i;
+        for (i = 1; i <= 100; i++) {
+            if ((i % 5 == 0) || (i % 7 == 0))
+                System.out.print(i + " ");
+        }*/
+
+
+        // Задача 6
+
+/*        int[] arrNum = {1, 2, 4, 6, 8, 10, 3, 5, 7, 11};
+
+        int sumArrNum = 0;
+        for (int i = 0; i < arrNum.length; i++) {
+            System.out.print(arrNum[i] + " ");
+            sumArrNum += arrNum[i];
+        }
+        System.out.print("Сумма элементов массива = " + sumArrNum);*/
+
+
+/*        int[] arrNum = {1, 2, 4, 6, 8, 10, 3, 5, 7, 11};
+
+        int sumArrNum = 0;
+        for (int digit : arrNum) {
+            sumArrNum += digit;
+        }
+        System.out.print("Сумма элементов массива = " + sumArrNum);*/
+
+
+        // Задача 7
+
+//        for(int i = 1; i <= 10; i++) {
+//            for(int j = 1; j <= 10; j++) {
+//                System.out.print(i * j + "    ");
+//            }
+//            System.out.println();
+//        }
+
+    }
+
+}
