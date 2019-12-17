6e1cb64b01d721642f36c1ce1503c8af84548279
Pavel <pishchalaups@belhard.com>
2019-12-17 17:52:29 +0300

    home work 3 part 5

--- hello_world/src/by/belhard/j19/homeworks/homeworkto02lessons/HomeWork03.java
+++ hello_world/src/by/belhard/j19/homeworks/homeworkto02lessons/HomeWork03.java
@@ -94,7 +94,7 @@ public class HomeWork03 {
 
 /*        int i;
         for (i = 1; i <= 100; i++) {
-            if (((i % 5 == 0) || (i % 7 == 0)) && i != 35)
+            if (((i % 5 == 0) && !(i % 7 == 0)) || ((i % 7 == 0) && !(i % 5 == 0)))
                 System.out.print(i + " ");
         }*/
 
